47ea0b4968d2a20d6958a54497ed51019b69311e
Anfrei Tkach <mr.tkach.a@gmail.com>
2019-08-23 18:10:39 +0300

    23/08 evenign commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,9 @@
 $(function(){
-
-
-  
+  $('.header__link_products').mouseenter(function () {
+    $('.header__dropdown').css('display', 'flex');
+  });
+  $('.header__link_products', '.header__dropdown').mouseout(function () {
+    $('.header__dropdown').css('display', 'none');
+});
+ 
 });
