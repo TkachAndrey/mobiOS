3df04d0d37a416d9d9597091ebe8a24eaec60c5b
Anfrei Tkach <mr.tkach.a@gmail.com>
2019-08-29 18:11:37 +0300

    mobile version 50%

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,32 @@
-$(function(){
-
+$(function () {
+  // ////////////////////////////////////
   $('.slider').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
-    fade: true, 
+    fade: true,
     dots: true,
     asNavFor: '.work__steps',
-    customPaging : function() {
+    customPaging: function () {
       return '<button class="slider__dots-item"></button>';
-    }
+    },
+    mobileFirst: true,
+    responsive: [{
+        breakpoint: 320,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          fade: false
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          fade: true
+        }
+      }
+    ]
   });
 
   $('.work__steps').slick({
@@ -20,23 +37,29 @@ $(function(){
     focusOnSelect: true
   });
   // ///////////////////////////////
-  // $(".form__needs-list_products").chosen({
-  //   placeholder_text_multiple: "Select an Option"
-  // });
+
 
   // //////////////////////////////
-  $('#FCA').on('change', function() {
+  $('#FCA').on('change', function () {
     $('#portName').attr("placeholder", "Port name").removeAttr("disabled");
   });
-  $('#DAP').on('change', function() {
+  $('#DAP').on('change', function () {
     $('#portName').attr("placeholder", "Port name").removeAttr("disabled");
   });
-  $('#FOB').on('change', function() {
+  $('#FOB').on('change', function () {
     $('#portName').attr("placeholder", "Delivery place").removeAttr("disabled");
   });
-  $('#CFR').on('change', function() {
+  $('#CFR').on('change', function () {
     $('#portName').attr("placeholder", "Delivery place").removeAttr("disabled");
   });
-
+  // ////////////////////
+  var $hamburger = $(".hamburger");
+  $hamburger.on("click", function (e) {
+    $hamburger.toggleClass("is-active");
+  });
+  ////////////////////////
+  $(".hamburger").on("click", function () {
+    $(".header__nav-mob").toggleClass("header__nav-mob_visible");
+  });
 
 });
